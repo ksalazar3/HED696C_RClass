69229108afa20ac7f9c8706128df3b6982627b02
Karina Salazar <ksalazar@email.arizona.edu>
2022-09-19 15:10:38 -0700

    mod3

---
+++ modules/module3/module3.R
@@ -0,0 +1,24 @@
+# Module 3
+
+# LOGISTICS
+
+    # Settings
+    rm(list = ls()) #removes all objects from your working environment
+    
+    # Load libraries
+      #install.packages("tidyverse") 
+      library(tidyverse)
+    
+    # Load dataframes used today
+      #load dataset with one obs per recruiting event
+      load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/recruiting/recruit_event_somevars.RData"))
+
+      #load dataset with one obs per high school
+      load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/recruiting/recruit_school_somevars.RData"))
+
+      
+# TIDYVERSE: STUDENT EXERCISE (write your answers here)
+      
+      
+
+      
