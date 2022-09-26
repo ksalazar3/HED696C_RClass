149eeb5738c086347b0cafff6a7d75c75273ad4a
Karina Salazar <ksalazar@email.arizona.edu>
2022-09-26 13:48:19 -0700

    mod4

---
+++ modules/module4/module4.R
@@ -0,0 +1,46 @@
+# Module 4
+
+# LOGISTICS
+
+    # Settings
+    rm(list = ls()) #removes all objects from your working environment
+    
+    # Load libraries
+      #install.packages("tidyverse") 
+      library(tidyverse)
+    
+    # Load dataframe used today
+      
+      #load student list dataset for Western Wash Univ
+      load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/prospect_list/wwlist_merged.RData"))
+      
+
+      
+# Student Exercises with Pipes
+      
+      
+      #write your code here
+      
+
+# Student Exercises using mutate()
+      
+      
+      #write your code here      
+      
+      
+# Student Exercises using ifelse()
+      
+      
+      #write your code here  
+      
+
+# Student Exercises using mutate()
+      
+      
+      #write your code here
+      
+      
+# Student Exercises using case_when()
+      
+      
+      #write your code here
