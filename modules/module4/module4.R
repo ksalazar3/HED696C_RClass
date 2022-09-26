832eb07ee14d291f345eafedc593238050588621
Karina Salazar <ksalazar@email.arizona.edu>
2022-09-26 14:14:33 -0700

    ps4

--- modules/module4/module4.R
+++ modules/module4/module4.R
@@ -14,7 +14,12 @@
       #load student list dataset for Western Wash Univ
       load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/prospect_list/wwlist_merged.RData"))
       
-
+      #load dataset with one obs per recruiting event
+      load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/recruiting/recruit_event_somevars.RData"))
+      
+      #load dataset with one obs per high school
+      load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/recruiting/recruit_school_allvars.RData"))
+      
       
 # Student Exercises with Pipes
       
