ff5db61077008913e2ea94c8d661879f26ac3f4a
Karina Salazar <ksalazar@email.arizona.edu>
2022-10-17 13:02:30 -0700

    updates

--- modules/module4/module4.R
+++ modules/module4/module4.R
@@ -20,6 +20,31 @@
       #load dataset with one obs per high school
       load(url("https://github.com/ksalazar3/HED696C_RClass/raw/master/data/recruiting/recruit_school_allvars.RData"))
       
+      #create new version of df_school_all with new variable names
+      school_v2 <- df_school_all %>% 
+        select(-contains("inst_")) %>% # remove vars that start with "inst_"
+        rename(
+          visits_by_berkeley = visits_by_110635,
+          visits_by_boulder = visits_by_126614,
+          visits_by_bama = visits_by_100751,
+          visits_by_stonybrook = visits_by_196097,
+          visits_by_rutgers = visits_by_186380,
+          visits_by_pitt = visits_by_215293,
+          visits_by_cinci = visits_by_201885,
+          visits_by_nebraska = visits_by_181464,
+          visits_by_georgia = visits_by_139959,
+          visits_by_scarolina = visits_by_218663,
+          visits_by_ncstate = visits_by_199193,
+          visits_by_irvine = visits_by_110653,
+          visits_by_kansas = visits_by_155317,
+          visits_by_arkansas = visits_by_106397,
+          visits_by_sillinois = visits_by_149222,
+          visits_by_umass = visits_by_166629,
+          num_took_read = num_took_rla,
+          num_prof_read = num_prof_rla,
+          med_inc = avgmedian_inc_2564)
+      
+      
       
 # Student Exercises with Pipes
       
